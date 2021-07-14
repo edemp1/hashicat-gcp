dec60522f83778cbe79ddf4f81570a3811a66a7a
Ed Kryda <ed.kryda@gfs.com>
2021-07-14 20:23:00 +0000

    Added vpc module2

--- vpc.tf
+++ vpc.tf
@@ -10,12 +10,12 @@ module "vpc" {
     {
       subnet_name   = "subnet-01"
       subnet_ip     = "10.10.10.0/24"
-      subnet_region = "us-west1"
+      subnet_region = "us-cemtral1"
     },
     {
       subnet_name           = "subnet-02"
       subnet_ip             = "10.10.20.0/24"
-      subnet_region         = "us-west1"
+      subnet_region         = "us-central1"
       subnet_private_access = "true"
       subnet_flow_logs      = "true"
       description           = "This subnet has a description"
@@ -23,7 +23,7 @@ module "vpc" {
     {
       subnet_name               = "subnet-03"
       subnet_ip                 = "10.10.30.0/24"
-      subnet_region             = "us-west1"
+      subnet_region             = "us-central1"
       subnet_flow_logs          = "true"
       subnet_flow_logs_interval = "INTERVAL_10_MIN"
       subnet_flow_logs_sampling = 0.7
@@ -56,7 +56,7 @@ module "vpc" {
       destination_range      = "10.50.10.0/24"
       tags                   = "app-proxy"
       next_hop_instance      = "app-proxy-instance"
-      next_hop_instance_zone = "us-west1-a"
+      next_hop_instance_zone = "us-central1-a"
     },
   ]
 }
